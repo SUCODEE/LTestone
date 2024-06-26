3656a796d1037d6f4ca3bdeacbfe81d3fc8849bf
SUCODEE <sucodeins@gmail.com>
2024-06-26 11:40:56 +0200

    one

---
+++ Sources/LTestone/inputText.swift
@@ -0,0 +1,13 @@
+ 
+import SwiftUI
+
+public struct inputText: View {
+    var title:String
+    public init(title: String) {
+        self.title = title
+    }
+public  var body: some View {
+        Text(title)
+    }
+}
+ 
